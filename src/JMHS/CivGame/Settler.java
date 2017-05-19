e3ceff27c1de927bb532bcd5f72b41e967c7a1b1
jcalem <jcalem@10.122.186.38>
2017-05-19 09:51:02 -0400

    Ryan is a goober

--- src/JMHS/CivGame/Settler.java
+++ src/JMHS/CivGame/Settler.java
@@ -1,7 +1,7 @@
 package JMHS.CivGame;
 
 public class Settler extends Unit {
-	// Ryan Brown is a goober
+	
 	public Settler(int locx, int locy, Civilization civ) {
 		super(locx, locy, civ);
 		// TODO Auto-generated constructor stub
